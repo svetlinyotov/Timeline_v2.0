0cbc96ea3dfe0da943a4407443b8f58a75ea3660
SvetlinYotov <svetlin.yotov@gmail.com>
2015-12-25 23:28:14 +0200

    Added functions for basic date time string formatting

--- app/Common.php
+++ app/Common.php
@@ -8,4 +8,24 @@ class Common
     {
         return \DateTimeZone::listIdentifiers(\DateTimeZone::ALL);
     }
+
+    public static function formatDateTimeForSQL($time) : string
+    {
+        return date('Y-m-d H:i:s', strtotime($time));
+    }
+
+    public static function formatDateTimeFromSQL($time) : string
+    {
+        return date('jS M Y, h:iA', strtotime($time));
+    }
+
+    public static function formatTimeForSQL($time) : string
+    {
+        return date('H:i:s', strtotime($time));
+    }
+
+    public static function formatTimeFromSQL($time) : string
+    {
+        return date('h:i A', strtotime($time));
+    }
 }
