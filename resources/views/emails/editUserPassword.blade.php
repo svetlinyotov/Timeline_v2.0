9d4b6c4fa4890cabbdbf1d9d3194fac3280ad214
SvetlinYotov <svetlin.yotov@gmail.com>
2015-12-30 22:56:23 +0200

    Emails viewes added

---
+++ resources/views/emails/editUserPassword.blade.php
@@ -0,0 +1,15 @@
+Dear {{$info->names}}, <br>
+Administrator from the company <b>{{$company}}</b> has updated your personal details in the TIMELINE online part-time job managing system.<br>
+You have new generated password. In order to log in the system you should go to <a href="http://timeline.snsdevelop.com">this</a> url: <a href="http://timeline.snsdevelop.com">http://timeline.snsdevelop.com</a>. <br><br>
+-----<b>New Login details</b>----- <br>
+Email: {{$user->email}}<br>
+Password: {{$password}}<br>
+---------------------------------- <br>
+<br>
+If you have any questions, do not hesitate to <a href="http://timeline.snsdevelop.com/support?rel=new_user_email">contact us</a>.
+We recommend you to change your password once you sign in. By signing in you accept out <a href="http://timeline.snsdevelop.com/terms">terms of use</a>.
+Once you login check your pre-entered details about you in your profile.<br><br>
+TIMELINE Support Team
+<br><br>
+<small>This is automatically generated email. Please do not answer it. This email can't receive replies.  To give us feedback on this alert, <a href="http://timeline.snsdevelop.com/support?rel=new_user_email">click here</a>.</small><br>
+<small>You received this mandatory email service announcement to update you about updated password in our platform TIMELINE.</small>
---
+++ resources/views/emails/newUserPassword.blade.php
@@ -0,0 +1,15 @@
+Dear {{$info->names}}, <br>
+The company <b>{{$company}}</b> has added you to the TIMELINE online part-time job managing system as a <i>{{$user->role}}</i>.<br>
+In order to log in the system you should go to <a href="http://timeline.snsdevelop.com">this</a> url: <a href="http://timeline.snsdevelop.com">http://timeline.snsdevelop.com</a>. <br><br>
+-----<b>Login details</b>----- <br>
+Username/email: {{$user->email}}<br>
+Password: {{$password}}<br>
+----------------------- <br>
+<br>
+If you have any questions, do not hesitate to <a href="http://timeline.snsdevelop.com/support?rel=new_user_email">contact us</a>.
+We recommend you to change your password once you sign in. By signing in you accept out <a href="http://timeline.snsdevelop.com/terms">terms of use</a>.
+Once you login check your pre-entered details about you in your profile.<br><br>
+TIMELINE Support Team
+<br><br>
+<small>This is automatically generated email. Please do not answer it. This email can't receive replies.  To give us feedback on this alert, <a href="http://timeline.snsdevelop.com/support?rel=new_user_email">click here</a>.</small><br>
+<small>You received this mandatory email service announcement to update you about new registration in our platform TIMELINE.</small>
