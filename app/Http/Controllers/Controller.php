f88ab92fc41c4de6c3396281472024bfe01fb32f
SvetlinYotov <svetlin.yotov@gmail.com>
2015-12-30 22:47:38 +0200

    Notification seen and timezone update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Notification;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Config;
 
 abstract class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function __construct(Request $request)
+    {
+        $timezone = 'UTC';
+        if(Auth::user()->company != null) $timezone = Auth::user()->company->timezone;
+
+        if(Auth::check()) {
+            Config::set('app.timezone', $timezone);
+        }
+
+        if($request->get('noti')) {
+            Notification::markAsRead($request->get('noti'));
+        }
+    }
 }
