d72a0f86e436d86090ed74fffd6e71a01c7c19c5
SvetlinYotov <svetlin.yotov@gmail.com>
2015-12-30 22:42:54 +0200

    Auth update

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -23,7 +23,7 @@ class AuthController extends Controller
 
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
-    protected $redirectPath = '/dashboard';
+    protected $redirectPath = '/profile';
     protected $loginPath = '/login';
 
     /**
