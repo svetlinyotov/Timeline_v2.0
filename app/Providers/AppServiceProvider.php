d522f65b11ee5d096bbf938ab801dfcdd3f9cc6e
SvetlinYotov <svetlin.yotov@gmail.com>
2015-12-30 22:50:55 +0200

    Added variables to master view

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Notification;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        view()->composer('layouts.master', function($view)
+        {
+            $view->with('user_notification_count', Notification::count(Auth::user()->id));
+            $view->with('user_notification_list', Notification::read(Auth::user()->id));
+        });
     }
 
     /**
