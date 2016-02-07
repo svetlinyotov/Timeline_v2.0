607308380b3ea7e14b43fb6a2601f350dd2c6f51
SvetlinYotov <svetlin.yotov@gmail.com>
2016-02-08 01:30:14 +0200

    Bug fixes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,12 +19,12 @@ class AppServiceProvider extends ServiceProvider
         view()->composer('layouts.master', function($view)
         {
             $view->with('user_notification_count', Notification::count(Auth::user()->id));
-            $view->with('user_notification_list', Notification::read(Auth::user()->id));
+            $view->with('user_notification_list', Notification::read(Auth::user()->id, true, 3));
         });
 
         view()->composer('users.list', function($view){
-            $view->with('company_id', User::where('id', Auth::user()->id)->with(['company' => function($q){$q->select('companies.id', 'companies.name');}])->first()->company->pluck('id')->toArray()[0]);
-            $view->with('company_name', User::where('id', Auth::user()->id)->with(['company' => function($q){$q->select('companies.id', 'companies.name');}])->first()->company->pluck('name')->toArray()[0]);
+            $view->with('company_id', User::where('id', Auth::user()->id)->with(['company' => function($q){$q->select('companies.id', 'companies.name');}])->first()->company->pluck('id')->toArray()[0]??"");
+            $view->with('company_name', User::where('id', Auth::user()->id)->with(['company' => function($q){$q->select('companies.id', 'companies.name');}])->first()->company->pluck('name')->toArray()[0]??"");
         });
     }
 
