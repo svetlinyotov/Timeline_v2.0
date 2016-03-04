be0049158c1eda0b70e1c36cf4f52182dba866a2
SvetlinYotov <svetlin.yotov@gmail.com>
2016-03-04 12:12:17 +0200

    General fixes

--- resources/views/emails/editUserPassword.blade.php
+++ resources/views/emails/editUserPassword.blade.php
@@ -9,7 +9,7 @@ Password: {{$password}}<br>
 If you have any questions, do not hesitate to <a href="http://timeline.snsdevelop.com/support?rel=new_user_email">contact us</a>.
 We recommend you to change your password once you sign in. By signing in you accept out <a href="http://timeline.snsdevelop.com/terms">terms of use</a>.
 Once you login check your pre-entered details about you in your profile.<br><br>
-TIMELINE Support Team
+TimeTracker Support Team
 <br><br>
-<small>This is automatically generated email. Please do not answer it. This email can't receive replies.  To give us feedback on this alert, <a href="http://timeline.snsdevelop.com/support?rel=new_user_email">click here</a>.</small><br>
-<small>You received this mandatory email service announcement to update you about updated password in our platform TIMELINE.</small>
+<small>This is automatically generated email. Please do not answer it. This email can't receive replies.  To give us feedback on this alert, <a href="http://timetracker.snsdevelop.com/support?rel=new_user_email">click here</a>.</small><br>
+<small>You received this mandatory email service announcement to update you about updated password in our platform TimeTracker.</small>
