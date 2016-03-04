58a3614719ad8209dc4ab05bfb17af46e82740bc
SvetlinYotov <svetlin.yotov@gmail.com>
2016-03-04 12:12:59 +0200

    Password reset added

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'info@timetracker.snsdevelop.com', 'name' => 'TimeTracker Team'],
 
     /*
     |--------------------------------------------------------------------------
